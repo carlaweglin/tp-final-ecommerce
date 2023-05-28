fe98602113e1934c402966d441f5d936f44b6100
carlaweglin <weglin.carla@gmail.com>
2023-05-27 22:38:10 -0300

    initial config firebase

--- src/components/NavApp.jsx
+++ src/components/NavApp.jsx
@@ -3,20 +3,23 @@ import { Button, HStack, Link } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 
 export function NavApp() {
+  
+  
+
+
+
+
   return (
-    <HStack w="100%" justifyContent='center' gap={100}>
+    <HStack w="100%" justifyContent="center" gap={100}>
       <Link as={RouterLink} to="/">
-      <Button colorScheme='blackAlpha'>Inicio</Button>
+        <Button colorScheme="blackAlpha">Inicio</Button>
       </Link>
       <Link as={RouterLink} to="/products">
-      <Button colorScheme='blackAlpha'>Productos</Button>
+        <Button colorScheme="blackAlpha">Productos</Button>
       </Link>
-
-      <Link as={RouterLink} to="/cart">
-      <Button colorScheme='blackAlpha'>Carrito</Button>
-      </Link>
+      <Button colorScheme="blackAlpha">Carrito</Button>
       <Link as={RouterLink} to="/login">
-      <Button colorScheme='blackAlpha'>Iniciar Sesion</Button>
+        <Button colorScheme="blackAlpha">Iniciar Sesion</Button>
       </Link>
     </HStack>
   )
