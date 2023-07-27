f96b9751a0b2d672915396622275a80d2aaec87d
carlaweglin <weglin.carla@gmail.com>
2023-07-27 20:13:04 -0300

    add change info in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,8 @@ import { NavApp } from './NavApp'
 
 export function Header() {
   return (
-    <VStack
-      justifyContent="space-around"
-      w="full"
-      
-      boxShadow="2xl"
-      
-    >
+    <VStack justifyContent="space-around" w="full" boxShadow="2xl">
+      <Stack bg="black" color="white" w="full" textAlign="center" ><div> 🚚Envio gratis | ⚡Retiros en 24hs | 💳Hasta 12 cuotas sin interés</div></Stack>
       <Stack textAlign="left">
         <Heading as="h1" color="black" textAlign="center">
           Tienda ADA
